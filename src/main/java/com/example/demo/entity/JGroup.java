9d6d928aa46700377b83752958fddba2a5d28d97
miorajulia01 <miorahanitra01.com>
2026-08-15 04:41:34 +0300

    feat(jpa): add Group model and JGroup entity

---
+++ src/main/java/com/example/demo/entity/JGroup.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "\"group\"")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+}
