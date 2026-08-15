38070cbfd948ff5baa598f7e6a707d4a89545ad2
miorajulia01 <miorahanitra01.com>
2026-08-15 04:35:08 +0300

    feat(jpa): add User model and JUser entity

---
+++ src/main/java/com/example/demo/entity/JUser.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "\"user\"")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String email;
+    private String role;
+}
