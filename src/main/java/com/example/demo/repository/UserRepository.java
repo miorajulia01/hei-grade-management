be55bd81fe26a2f20eabea209c251d6489f412ae
miorajulia01 <miorahanitra01.com>
2026-08-16 05:39:02 +0300

    feat(repository): add UserRepository interface

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.JUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<JUser, String> {
+    Optional<JUser> findByEmail(String email);
+}
