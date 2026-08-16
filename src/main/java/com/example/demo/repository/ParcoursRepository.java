01f72af752606bba701c616b5a166f6d23554561
miorajulia01 <miorahanitra01.com>
2026-08-16 05:48:02 +0300

    feat(repository): add ParcoursRepository interface

---
+++ src/main/java/com/example/demo/repository/ParcoursRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.JParcours;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParcoursRepository extends JpaRepository<JParcours, String> {
+}
