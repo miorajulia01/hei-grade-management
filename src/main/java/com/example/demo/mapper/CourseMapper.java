1bc539501f7187f78f4bdd1cafab8206bc56ba6e
miorajulia01 <miorahanitra01.com>
2026-08-16 06:10:30 +0300

    feat(mapper): add CourseMapper component

---
+++ src/main/java/com/example/demo/mapper/CourseMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.JCourse;
+import com.example.demo.model.Course;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseMapper {
+    public Course toModel(JCourse entity) {
+        if (entity == null) return null;
+        return Course.builder()
+                .id(entity.getId())
+                .code(entity.getCode())
+                .name(entity.getName())
+                .credits(entity.getCredits())
+                .build();
+    }
+}
