8c61cd515f564577d03bf966db2a42fda9cf355b
miorajulia01 <miorahanitra01.com>
2026-08-16 06:09:46 +0300

    feat(mapper): add ParcoursMapper component

---
+++ src/main/java/com/example/demo/mapper/ParcoursMapper.java
@@ -0,0 +1,16 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.JParcours;
+import com.example.demo.model.Parcours;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParcoursMapper {
+    public Parcours toModel(JParcours entity) {
+        if (entity == null) return null;
+        return Parcours.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .build();
+    }
+}
