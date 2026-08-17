7124f64aa60997e8891ee06485616fc7945d4c55
miorajulia01 <miorahanitra01.com>
2026-08-17 14:14:10 +0300

    fix(repository): add findByName to SemesterRepository

--- src/main/java/com/example/demo/repository/SemesterRepository.java
+++ src/main/java/com/example/demo/repository/SemesterRepository.java
@@ -4,5 +4,9 @@ import com.example.demo.entity.JSemester;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface SemesterRepository extends JpaRepository<JSemester, String> {}
+public interface SemesterRepository extends JpaRepository<JSemester, String> {
+    Optional<JSemester> findByName(String name);
+}
