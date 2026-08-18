7cdb9ee5945d797527777ebaf870c79d463fb7bd
miorajulia01 <miorahanitra01.com>
2026-08-18 10:21:16 +0300

    feat(service): integrate ExamValidator in ExamService

--- src/main/java/com/example/demo/service/ExamService.java
+++ src/main/java/com/example/demo/service/ExamService.java
@@ -6,6 +6,7 @@ import com.example.demo.mapper.ExamMapper;
 import com.example.demo.model.Exam;
 import com.example.demo.repository.CourseRepository;
 import com.example.demo.repository.ExamRepository;
+import com.example.demo.validator.ExamValidator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,32 +24,34 @@ public class ExamService {
 
   public Exam getExamById(String id) {
     JExam entity =
-        examRepository
-            .findById(id)
-            .orElseThrow(() -> new RuntimeException("Exam not found with id: " + id));
+            examRepository
+                    .findById(id)
+                    .orElseThrow(() -> new RuntimeException("Exam not found with id: " + id));
     return ExamMapper.toModel(entity);
   }
 
   public Exam saveExam(Exam model) {
+    ExamValidator.validate(model);
+
     JCourse course = null;
     if (model.getCourse() != null && model.getCourse().getId() != null) {
       course =
-          courseRepository
-              .findById(model.getCourse().getId())
-              .orElseThrow(() -> new RuntimeException("Course not found"));
+              courseRepository
+                      .findById(model.getCourse().getId())
+                      .orElseThrow(() -> new RuntimeException("Course not found"));
     }
 
     JExam entity =
-        JExam.builder()
-            .id(model.getId())
-            .course(course)
-            .type(model.getType())
-            .title(model.getTitle())
-            .dateExam(model.getDateExam())
-            .coefficient(model.getCoefficient())
-            .order(model.getOrder())
-            .isPublished(model.getIsPublished())
-            .build();
+            JExam.builder()
+                    .id(model.getId())
+                    .course(course)
+                    .type(model.getType())
+                    .title(model.getTitle())
+                    .dateExam(model.getDateExam())
+                    .coefficient(model.getCoefficient())
+                    .order(model.getOrder())
+                    .isPublished(model.getIsPublished())
+                    .build();
 
     JExam saved = examRepository.save(entity);
     return ExamMapper.toModel(saved);
