61d0e41f04b59ccfdeaec1b7c71ca3e7d16f7b61
miorajulia01 <miorahanitra01.com>
2026-08-20 19:13:20 +0300

    feat: add error response

---
+++ src/main/java/com/example/demo/exception/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.example.demo.exception;
+
+import java.time.Instant;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+  private Instant timestamp;
+  private int status;
+  private String error;
+  private String message;
+  private String path;
+}
